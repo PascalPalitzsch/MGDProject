df17b55917c8bd6bcf302db84d9e0184c3451390
Pascal Palitzsch <pascal.palitzsch@web.de>
2024-05-14 12:17:43 +0200

    added Ground check

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,8 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     private float Move;
-
     public float jump;
+    public bool isJumping;
 
     private Rigidbody2D rb;
     // Start is called before the first frame update
@@ -23,9 +23,26 @@ public class PlayerMovement : MonoBehaviour
 
         rb.velocity = new Vector2(speed * Move, rb.velocity.y);
 
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && isJumping == false)
         {
             rb.AddForce(new Vector2(rb.velocity.x, jump));
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
+    }
+
 }
